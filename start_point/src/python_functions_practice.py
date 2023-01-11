1d177eb90a43e7fa6659a429c5a6aff847bdc354
Mathias Kerr <mathiasfkerr@gmail.com>
2023-01-11 16:04:58 +0000

    mvp finished

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,32 @@
+def return_10():
+    return 10
+
+def add(num_1,num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2    
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(test_string):
+    return len(test_string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(num_1, num_2):
+    return int(num_1) + int(num_2)
+
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+def number_to_full_month_name(month):
+    return months[month-1]
+
+def number_to_short_month_name(month):
+    return months[month-1][:3]   
+
